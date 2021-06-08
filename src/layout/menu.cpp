fb3071d5a88515a11335145ed3bbbf6a02db9e2f
unknown <hjofre24@gmail.com>
2021-06-07 23:07:04 -0300

    Modification of the sign in the menu

--- src/layout/menu.cpp
+++ src/layout/menu.cpp
@@ -43,6 +43,7 @@ void menu_puntos();
 void menu_dificultad();
 void cancion_1();
 void loader();
+void write();
  
 int main()
 {
@@ -56,6 +57,8 @@ void menu_principal()
 {
    bool repite = true;
    int opcion;   
+   
+   
  	
    // Titulo del menú y nombres de las opciones
    const char *titulo = "MENU PRINCIPAL";
@@ -84,7 +87,8 @@ void menu_principal()
          	Beep(700,30);
          	Beep(600,50);
          	Beep(1000,30);
-            cout << "\nGRACIAS POR JUGAR !! ADIOS" << endl;
+         	gotoxy(35,25);
+            cout << " GRACIAS POR JUGAR !! ADIOS" << endl;
             repite = false;
             system("pause>nul");
             break;
@@ -165,8 +169,8 @@ void menu_dificultad()
    int opcion;
    // Titulo del menú y nombres de las opciones
    const char *titulo = "MENU DE DIFICULTAD";
-   const char *opciones[] = {"FACIL","MEDIO","DIFICIL","ROMPEME EL ORTO","ATRAS"};
-   int numopciones = 5;  // Numero de opciones
+   const char *opciones[] = {"FACIL","MEDIO","DIFICIL","ATRAS"};
+   int numopciones = 4;  // Numero de opciones
  
    do {
       opcion = menu(titulo, opciones, numopciones);
@@ -174,27 +178,22 @@ void menu_dificultad()
       system("cls");
       switch (opcion) {
          case 1:
-            cout << "numero1 = ";
+            cout << " LA DIFICULTAD FUE CAMBIADA A FACIL ";
             system("pause>nul");
             break;
  
  
          case 2:
-            cout << "numero1 = ";
+            cout << " LA DIFICULTAD FUE CAMBIADA A MEDIO ";
             system("pause>nul");
             break;
             
  		 case 3:
-            cout << "numero1 = ";
+            cout << " LA DIFICULTAD FUE CAMBIADA A DIFICIL ";
             system("pause>nul");
             break;
             
          case 4:
-            cout << "numero1 = ";
-            system("pause>nul");
-            break;  
-			 
-         case 5:
             repite = false;
             break;
       }
@@ -209,18 +208,21 @@ int menu(const char titulo[], const char *opciones[], int numopciones)
    int tecla;
  
    bool repite = true; // controla el bucle para regresar a la rutina que lo llamo, al presionar ENTER
+   
  
    do {
+   		 
       system("cls");
       system("color 0a");
-      gotoxy(40, 6 + opcionSeleccionada); cout << "==>" << endl;
+      write();
+      gotoxy(40, 15 + opcionSeleccionada); cout << "==>" << endl;
  
       // Imprime el título del menú
-      gotoxy(47, 8); cout << titulo;
+      gotoxy(47, 14); cout << titulo;
  
       // Imprime las opciones del menú
       for (int i = 0; i < numopciones; ++i) {
-         gotoxy(47, 7 + i); cout << i + 1 << ") " << opciones[i];
+         gotoxy(47, 16 + i); cout << i + 1 << ") " << opciones[i];
       }
  
       // Solo permite que se ingrese ARRIBA, ABAJO o ENTER
@@ -327,20 +329,35 @@ void cancion_1()
 
 void loader(){
 	char caracter = 254;
+	char caracter1= 205;
 	char caracter2 = 176;
+	char caracter3 = 186;
+	
+	write();
+	
 	system("color 0a");
-	gotoxy (50,10);
-	cout<<" CROMOSOMAS"<<endl;
-	gotoxy (37,12);
+	gotoxy (50,16);
+	cout<<" CARGANDO"<<endl;
+	gotoxy (37,18);
 	cout<<"¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯"<<endl;
-	gotoxy (37,14);
+	gotoxy (37,20);
 	cout<<"¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯"<<endl;
+	
+	for (int j=2; j<117; j++){ //cuadro
+		gotoxy (j,3); cout<<caracter1;
+		gotoxy (j,30); cout<<caracter1;
+	}
+	
+	for (int k=4; k<30; k++){ //cuadro
+		gotoxy (2,k); cout<<caracter3;
+		gotoxy (116,k); cout<<caracter3;
+	}
 	
 	for (int i=38;i<74;i++){
 		system("color 02");
 		system("color 06");
 		system("color 09");
-		gotoxy (i,13);
+		gotoxy (i,19);
 		cout<<caracter;
 		
 		for (int j=1;j<3;j++){
@@ -350,5 +367,37 @@ void loader(){
 		}
 	}
 
+}
+
+void write (){
+	
+	char caracter1= 205;
+	char caracter3 = 186;
+	
+	for (int j=2; j<117; j++){ //cuadro
+		gotoxy (j,3); cout<<caracter1;
+		gotoxy (j,30); cout<<caracter1;
+	}
+	
+	for (int k=4; k<30; k++){ //cuadro
+		gotoxy (2,k); cout<<caracter3;
+		gotoxy (116,k); cout<<caracter3;
+	}
+	
+		gotoxy (20,6);
+	  cout<<" M7MMMMMq.    M7MMF'  M7MF `7MN.  M7MF'`7MN.  M7MF'` 7MMMMMYMM   M7MMMMMMq."<<endl;
+	 	 gotoxy (20,7);
+      cout<<"  MM   -MM.   MM       M    MMN.    M    MMN.    M    MM    -7    MM   -MM." <<endl;
+      	gotoxy (20,8);
+      cout<<"  MM   ,M9    MM       M    M YMb   M    M YMb   M    MM   d      MM   ,M9  "<<endl;
+     	gotoxy (20,9);
+      cout<<"  MMmmdM9     MM       M    M  -MN. M    M  -MN. M    MMmmMM      MMmmdM9 "  <<endl;
+     	 gotoxy (20,10);
+      cout<<"  MM  YM.     MM       M    M   -MM.M    M   -MM.M    MM   Y  ,   MM  YM.  " <<endl;
+     	 gotoxy (20,11);
+      cout<<"  MM   -Mb.   YM.     ,M    M     YMM    M     YMM    MM     ,M   MM   -Mb. "<<endl;
+    	 gotoxy (20,12);
+      cout<<"-.JMML. .JMM.   -bmmmmd'  .JML.    YM  .JML.    YM  .JMMmmmmMMM .JMML. .JMM.M"<<endl;
+	
 }
 
